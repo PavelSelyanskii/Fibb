07e2b48a46e5f957b8ac71b9a74344e21c35edc7
63266 <63266@DESKTOP-0GKU3O7>
2024-09-17 21:01:30 +0700

    Добавьте файлы проекта.

---
+++ Fibb/Program.cs
@@ -0,0 +1,49 @@
+﻿//фибоначчи
+// Задание 2
+
+//Random random = new Random();
+
+
+//int firstFib = random.Next(1, 10);  
+//int secondFib = random.Next(1, 10); 
+
+//Console.WriteLine($"Первое случайное число Фибоначчи: {firstFib}");
+//Console.WriteLine($"Второе случайное число Фибоначчи: {secondFib}");
+
+
+//int thirdFib = firstFib + secondFib;      
+//int fourthFib = secondFib + thirdFib;     
+//int fifthFib = thirdFib + fourthFib;      
+
+//Console.WriteLine($"Третье число Фибоначчи: {thirdFib}");
+//Console.WriteLine($"Четвёртое число Фибоначчи: {fourthFib}");
+//Console.WriteLine($"Пятое число Фибоначчи: {fifthFib}");
+
+
+//int sumFib = firstFib + secondFib + thirdFib + fourthFib + fifthFib;
+
+
+//int squareOfSum = sumFib * sumFib;
+
+//Console.WriteLine($"Сумма чисел Фибоначчи: {sumFib}");
+//Console.WriteLine($"Квадрат суммы чисел Фибоначчи: {squareOfSum}");
+
+
+////Задание 3
+
+//Console.WriteLine("Введите начало диапазона:");
+//int start = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Введите конец диапазона:");
+//int end = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Нечётные числа, которые делятся на 3 без остатка:");
+
+//for (int i = start; i <= end; i++)
+//{
+
+//    if (i % 2 != 0 && i % 3 == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
